6f9c06bf642678d0df59931ec83a7db6cdd16d08
TiboSarl <57560784+TiboSarl@users.noreply.github.com>
2022-05-17 20:30:11 -0400

    Initial Commit

---
+++ PokeRoyal/Source/PokeRoyal/PokeRoyalGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PokeRoyalGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APokeRoyalGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APokeRoyalGameMode();
+};
+
+
+
